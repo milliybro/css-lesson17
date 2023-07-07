70d4cc2128d3ccb667d6f13518b0d8cb007dd65f
“milliybro” <shohrux-rustamov@mail.ru>
2023-07-07 12:27:54 +0500

    third commit

--- js/main.js
+++ js/main.js
@@ -20,3 +20,50 @@ $('.owl-carousel').owlCarousel({
        },
    }
 });
+
+
+window.addEventListener("scroll", function () {
+   shrink();
+ });
+ 
+ let navbar = document.getElementById("navbar");
+ 
+ function shrink() {
+   if (scrollY > 100) {
+     navbar.classList.add("navbar-shrink");
+   } else {
+       navbar.classList.remove("navbar-shrink");
+    }
+ }
+
+ 
+function openNavbar() {
+   document.getElementById("navbar-responsive").style.left = "0";
+   // document.getElementById("navbar-responsive").style.top = "0";
+ }
+ function closeNavbar() {
+   document.getElementById("navbar-responsive").style.left = "-100%";
+   // document.getElementById("navbar-responsive").style.top = "-100%";
+ }
+ 
+ document.getElementById("navbar-open").addEventListener("click", openNavbar);
+ document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+ 
+ 
+ window.addEventListener("scroll", function () {
+   toggleBacktop();
+ });
+
+
+ let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    console.log("light");
+    this.firstElementChild.src = "../assets/images/light.svg";
+  } else {
+    console.log("dark");
+    this.firstElementChild.src = "../assets/images/night.svg";
+  }
+  document.body.classList.toggle("light");
+});
